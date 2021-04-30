70ddb1a364a44e913f06a4a87dd8a5d2e6d39835
Geoff Taylor <geoff@knife.opgeek.lan>
2021-04-30 15:20:59 +0100

    Added timestamp to logging.

--- meta/startup/projectsetup.py
+++ meta/startup/projectsetup.py
@@ -16,4 +16,4 @@ pd.options.display.float_format = '{:,.8f}'.format
 decimal.getcontext().prec = 18
 
 # Make logging a little more verbose than the default.
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)-8s %(name)-12s %(message)s")
